04017eed0b63f738af2aa7f69ed81cb4c6a3ae46
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-04-09 14:13:31 +0300

    create a function to get all posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,7 +2,7 @@ const Post = require("../models/postModel.js");
 const asyncHandler = require("express-async-handler");
 
 const createPost = asyncHandler(async (req, res) => {
-  const post = Post.create({
+  const post = await Post.create({
     PostTitle: req.body.PostTitle,
     PostType: req.body.PostType,
     PostDescription: req.body.PostDescription,
@@ -14,7 +14,16 @@ const createPost = asyncHandler(async (req, res) => {
     res.status(400).json("Something went wrong");
   }
 });
+const getPosts = asyncHandler(async (req, res) => {
+  const posts = await Post.find();
+  if (posts) {
+    res.status(200).json(posts);
+  } else {
+    res.status(400).json("Something went wrong");
+  }
+});
 
 module.exports = {
   createPost,
+  getPosts,
 };
