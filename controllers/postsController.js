c6cc06b982343bab2c81747b21691b5b9aa87efc
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-04-09 14:41:57 +0300

    create a function to delte posts

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const Post = require("../models/postModel.js");
 const asyncHandler = require("express-async-handler");
 
@@ -14,6 +16,7 @@ const createPost = asyncHandler(async (req, res) => {
     res.status(400).json("Something went wrong");
   }
 });
+
 const getPosts = asyncHandler(async (req, res) => {
   const posts = await Post.find();
   if (posts) {
@@ -23,7 +26,26 @@ const getPosts = asyncHandler(async (req, res) => {
   }
 });
 
+const deletePost = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params._id);
+
+  if (post) {
+    fs.unlink(path.join(__dirname, post.PostImage), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Failed to delete image file" });
+      }
+
+      post.remove();
+      res.status(200).json({ message: "Post deleted successfully" });
+    });
+  } else {
+    res.status(404).json({ message: "Post not found" });
+  }
+});
+
 module.exports = {
   createPost,
   getPosts,
+  deletePost,
 };
