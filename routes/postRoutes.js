d4f00d14a9cc6ecc55ed8b6b9d0f75e19a37a1f5
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-04-09 14:27:20 +0300

    install multer and create a route to hanlde incoming files

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const dir = path.join(__dirname, "../uploads");
+    fs.mkdirSync(dir, { recursive: true });
+    cb(null, dir);
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
 const { createPost, getPosts } = require("../controllers/postsController");
 
-router.post("/", createPost);
+router.post("/", upload.single("image"), createPost);
 router.get("/", getPosts);
 
 module.exports = router;
