1be62d0cc6ddf57f6160f58cf215872db96e2310
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-04-13 15:46:03 +0300

    create api for categories

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -11,11 +11,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use('/api/blog', require('./routes/blogRoutes'));
-app.use('/api/news', require('./routes/newsRoutes'));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use("/api/blog", require("./routes/blogRoutes"));
+app.use("/api/news", require("./routes/newsRoutes"));
+app.use("/api/blogCategory", require("./routes/blogCategoryRoutes"));
+app.use("/api/newsCategory", require("./routes/newsCategoryRoutes"));
 
-console.log('Server is listening on port', port);
+console.log("Server is listening on port", port);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
