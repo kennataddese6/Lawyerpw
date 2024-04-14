3dcd921516dd20df5aa161ca6634656a6ded02da
unknown <kennataddese6@gmail.com>
2024-04-14 04:45:32 -0700

    remove depreciated configrations and path log

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const path = require('path')
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -12,13 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-app.use("/api/blog", require("./routes/blogRoutes"));
-app.use("/api/news", require("./routes/newsRoutes"));
-app.use("/api/blogCategory", require("./routes/blogCategoryRoutes"));
-app.use("/api/newsCategory", require("./routes/newsCategoryRoutes"));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use('/api/blog', require('./routes/blogRoutes'));
+app.use('/api/news', require('./routes/newsRoutes'));
+app.use('/api/blogCategory', require('./routes/blogCategoryRoutes'));
+app.use('/api/newsCategory', require('./routes/newsCategoryRoutes'));
 
-console.log("Server is listening on port", port);
+console.log(path.join(__dirname, '/uploads'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
