19728f631e55e1b62db862abd5fe0ad58596120e
unknown <kennataddese6@gmail.com>
2024-04-14 05:38:42 -0700

    log directory contents when requested

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const connectDB = require('./config/db');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
-
+const fs = require('fs');
 dotenv.config();
 const port = process.env.PORT || 5000;
 connectDB();
@@ -12,7 +12,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use('/uploads', (req, res, next) => {
+  const dirPath = path.join(__dirname, '/uploads');
+
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      console.error(`Error reading directory: ${err}`);
+    } else {
+      console.log(`Directory contents: ${files}`);
+    }
+  });
+
+  express.static(dirPath)(req, res, next);
+});
 app.use('/api/blog', require('./routes/blogRoutes'));
 app.use('/api/news', require('./routes/newsRoutes'));
 app.use('/api/blogCategory', require('./routes/blogCategoryRoutes'));
