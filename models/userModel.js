9d47c0c7caab87eda6297286a14568924f64ab4b
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-04-17 18:06:01 +0300

    create a scehma for user and add secret key to  env

---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    secretKey: {
+      type: String,
+      default: process.env.SECRET_KEY,
+    },
+    roles: {
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
