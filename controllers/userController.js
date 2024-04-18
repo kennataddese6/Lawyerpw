d179d5a197afeb0d37cb61d8b10084fc9a9ff97e
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-04-18 10:18:40 +0300

    create functions for auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,37 @@ const generateToken = (id) =>
     // expiresIn: "10s",
   });
 
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, roles } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+    roles,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      roles: user.roles,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
@@ -26,7 +57,47 @@ const loginUser = asyncHandler(async (req, res) => {
     throw new Error("Incorrect Email or Password");
   }
 });
-
+const changePassword = asyncHandler(async (req, res) => {
+  const user = await User.findOne({ _id: req.body.Id });
+  if (user) {
+    const passwordMatch = await bcrypt.compare(
+      req.body.currentPassword,
+      user.Password
+    );
+    if (passwordMatch) {
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(
+        req.body.newPassword,
+        saltRounds
+      );
+      user.Password = hashedPassword;
+      await user.save();
+      res.status(200).json("Password Changed Successfully.");
+    } else {
+      res.status(401).json("Incorrect Password");
+    }
+  } else {
+    res.status(404).json("User not found");
+  }
+});
+const resetPassword = asyncHandler(async (req, res) => {
+  const user = await User.findOne({ Email: req.body.email });
+  if (user) {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(
+      process.env.RESET_PASSWORD,
+      saltRounds
+    );
+    user.Password = hashedPassword;
+    await user.save();
+    res.status(200).json("Password Changed Successfully.");
+  } else {
+    res.status(404).json("User not found");
+  }
+});
 module.exports = {
   loginUser,
+  registerUser,
+  changePassword,
+  resetPassword,
 };
