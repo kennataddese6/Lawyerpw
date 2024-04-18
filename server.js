2a58b0fb18e4482b47bb379deda1fe43bd4c1c28
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-04-18 10:42:12 +0300

    add user routes to api

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
+const fs = require("fs");
 dotenv.config();
 const port = process.env.PORT || 5000;
 connectDB();
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/uploads', (req, res, next) => {
-  const dirPath = path.join(__dirname, '/uploads');
+app.use("/uploads", (req, res, next) => {
+  const dirPath = path.join(__dirname, "/uploads");
 
   fs.readdir(dirPath, (err, files) => {
     if (err) {
@@ -25,12 +25,13 @@ app.use('/uploads', (req, res, next) => {
 
   express.static(dirPath)(req, res, next);
 });
-app.use('/api/blog', require('./routes/blogRoutes'));
-app.use('/api/news', require('./routes/newsRoutes'));
-app.use('/api/blogCategory', require('./routes/blogCategoryRoutes'));
-app.use('/api/newsCategory', require('./routes/newsCategoryRoutes'));
+app.use("/api/user", require("./routes/userRoutes"));
+app.use("/api/blog", require("./routes/blogRoutes"));
+app.use("/api/news", require("./routes/newsRoutes"));
+app.use("/api/blogCategory", require("./routes/blogCategoryRoutes"));
+app.use("/api/newsCategory", require("./routes/newsCategoryRoutes"));
 
-console.log(path.join(__dirname, '/uploads'));
+console.log(path.join(__dirname, "/uploads"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
