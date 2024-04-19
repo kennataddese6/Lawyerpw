67c3f075f63f9654dc4c32a0857911c3cbff3a29
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-04-19 15:18:55 +0300

    change the file path to url

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,21 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-const util = require('util');
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
 const unlinkAsync = util.promisify(fs.unlink);
-const Blog = require('../models/blogModel.js');
-const asyncHandler = require('express-async-handler');
+const Blog = require("../models/blogModel.js");
+const asyncHandler = require("express-async-handler");
 
 const createBlog = asyncHandler(async (req, res) => {
   const blog = await Blog.create({
     BlogTitle: req.body?.BlogTitle,
     BlogCategory: req.body?.BlogCategory,
     BlogDescription: req.body?.BlogDescription,
-    BlogImage: req.file?.path.replace('/opt/render/project/src', ''),
+    BlogImage: `${process.env.UPLOAD_PATH}` + req.file?.filename,
   });
   if (blog) {
     res.status(200).json(blog);
   } else {
-    res.status(400).json('Something went wrong');
+    res.status(400).json("Something went wrong");
   }
 });
 
@@ -24,7 +24,7 @@ const getBlogs = asyncHandler(async (req, res) => {
   if (blog) {
     res.status(200).json(blog);
   } else {
-    res.status(400).json('Something went wrong');
+    res.status(400).json("Something went wrong");
   }
 });
 
@@ -36,13 +36,13 @@ const deleteBlogs = asyncHandler(async (req, res) => {
       await unlinkAsync(blog.BlogImage);
 
       await Blog.deleteOne({ _id: req.body._id });
-      res.status(200).json({ message: 'Blog deleted successfully' });
+      res.status(200).json({ message: "Blog deleted successfully" });
     } catch (err) {
       console.error(err);
-      return res.status(500).json({ message: 'Failed to delete blog' });
+      return res.status(500).json({ message: "Failed to delete blog" });
     }
   } else {
-    res.status(404).json({ message: 'Blog not found' });
+    res.status(404).json({ message: "Blog not found" });
   }
 });
 module.exports = {
