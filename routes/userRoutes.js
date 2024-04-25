878850edb38bab88b3f674e915521ab278b22b24
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-04-25 16:51:33 +0300

    create a route to change forgotten apsswor

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,11 +8,13 @@ const {
   resetPassword,
   sendEmail,
   verifyToken,
+  changeForgottenPassword,
 } = require("../controllers/userController");
 
 router.route("/").post(registerUser).put(protect, changePassword);
 router.route("/login").post(loginUser);
 router.route("/verify").post(verifyToken);
+router.route("/forgot").post(changeForgottenPassword);
 router.route("/sendEmail").post(sendEmail);
 router.route("/reset").post(protect, resetPassword);
 module.exports = router;
