9ceb3889e02ccc93bed0e78f88ea1a0518b58b14
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-04-25 15:38:50 +0300

    create a route for sending emal

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,9 +6,11 @@ const {
   loginUser,
   changePassword,
   resetPassword,
+  sendEmail,
 } = require("../controllers/userController");
 
 router.route("/").post(registerUser).put(protect, changePassword);
 router.route("/login").post(loginUser);
+router.route("/sendEmail").post(sendEmail);
 router.route("/reset").post(protect, resetPassword);
 module.exports = router;
